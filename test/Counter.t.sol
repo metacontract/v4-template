b7a5c94c1d18db7d9666218e1af95769d7ab236e
0xHaku <32897506+0xHaku@users.noreply.github.com>
2024-11-16 17:38:52 +0700

    fix Counter.t.tsol

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -13,19 +13,31 @@ import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
 import {PoolSwapTest} from "v4-core/src/test/PoolSwapTest.sol";
 import {Counter} from "../src/Counter.sol";
 import {StateLibrary} from "v4-core/src/libraries/StateLibrary.sol";
+import {HookMiner} from "../test/utils/HookMiner.sol";
 
 import {LiquidityAmounts} from "v4-core/test/utils/LiquidityAmounts.sol";
 import {IPositionManager} from "v4-periphery/src/interfaces/IPositionManager.sol";
 import {EasyPosm} from "./utils/EasyPosm.sol";
 import {Fixtures} from "./utils/Fixtures.sol";
 
-contract CounterTest is Test, Fixtures {
+import {MCTest} from "@mc-devkit/MCTest.sol";
+import {Proxy as UCSProxy} from "@ucs.mc/proxy/Proxy.sol";
+
+import {Base} from "bundle/Counter/functions/Base.sol";
+import {AfterSwap} from "bundle/Counter/functions/AfterSwap.sol";
+import {BeforeAddLiquidity} from "bundle/Counter/functions/BeforeAddLiquidity.sol";
+import {BeforeRemoveLiquidity} from "bundle/Counter/functions/BeforeRemoveLiquidity.sol";
+import {BeforeSwap} from "bundle/Counter/functions/BeforeSwap.sol";
+import {CounterFacade} from "bundle/Counter/CounterFacade.sol";
+import {Reader} from "bundle/Counter/Reader.sol";
+
+contract CounterTest is MCTest, Fixtures {
     using EasyPosm for IPositionManager;
     using PoolIdLibrary for PoolKey;
     using CurrencyLibrary for Currency;
     using StateLibrary for IPoolManager;
 
-    Counter hook;
+    Reader hook;
     PoolId poolId;
 
     uint256 tokenId;
@@ -39,19 +51,10 @@ contract CounterTest is Test, Fixtures {
 
         deployAndApprovePosm(manager);
 
-        // Deploy the hook to an address with the correct flags
-        address flags = address(
-            uint160(
-                Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG
-                    | Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG
-            ) ^ (0x4444 << 144) // Namespace the hook to avoid collisions
-        );
-        bytes memory constructorArgs = abi.encode(manager); //Add all the necessary constructor arguments from the hook
-        deployCodeTo("Counter.sol:Counter", constructorArgs, flags);
-        hook = Counter(flags);
+        hook = Reader(deployHook());
 
         // Create the pool
-        key = PoolKey(currency0, currency1, 3000, 60, IHooks(hook));
+        key = PoolKey(currency0, currency1, 3000, 60, IHooks(address(hook)));
         poolId = key.toId();
         manager.initialize(key, SQRT_PRICE_1_1);
 
@@ -83,11 +86,11 @@ contract CounterTest is Test, Fixtures {
 
     function testCounterHooks() public {
         // positions were created in setup()
-        assertEq(hook.beforeAddLiquidityCount(poolId), 1);
-        assertEq(hook.beforeRemoveLiquidityCount(poolId), 0);
+        assertEq(hook.beforeAddLiquidityCount(), 1);
+        assertEq(hook.beforeRemoveLiquidityCount(), 0);
 
-        assertEq(hook.beforeSwapCount(poolId), 0);
-        assertEq(hook.afterSwapCount(poolId), 0);
+        assertEq(hook.beforeSwapCount(), 0);
+        assertEq(hook.afterSwapCount(), 0);
 
         // Perform a test swap //
         bool zeroForOne = true;
@@ -97,14 +100,14 @@ contract CounterTest is Test, Fixtures {
 
         assertEq(int256(swapDelta.amount0()), amountSpecified);
 
-        assertEq(hook.beforeSwapCount(poolId), 1);
-        assertEq(hook.afterSwapCount(poolId), 1);
+        assertEq(hook.beforeSwapCount(), 1);
+        assertEq(hook.afterSwapCount(), 1);
     }
 
     function testLiquidityHooks() public {
         // positions were created in setup()
-        assertEq(hook.beforeAddLiquidityCount(poolId), 1);
-        assertEq(hook.beforeRemoveLiquidityCount(poolId), 0);
+        assertEq(hook.beforeAddLiquidityCount(), 1);
+        assertEq(hook.beforeRemoveLiquidityCount(), 0);
 
         // remove liquidity
         uint256 liquidityToRemove = 1e18;
@@ -118,7 +121,45 @@ contract CounterTest is Test, Fixtures {
             ZERO_BYTES
         );
 
-        assertEq(hook.beforeAddLiquidityCount(poolId), 1);
-        assertEq(hook.beforeRemoveLiquidityCount(poolId), 1);
+        assertEq(hook.beforeAddLiquidityCount(), 1);
+        assertEq(hook.beforeRemoveLiquidityCount(), 1);
     }
+
+        function deployHook() public returns (address) {
+        mc.init("Counter");
+        Base base = new Base();
+        mc.use("getHookPermissions", Base.getHookPermissions.selector, address(base));
+        mc.use("validateHookAddress", Base.validateHookAddress.selector, address(base));
+        mc.use("AfterSwap", AfterSwap.afterSwap.selector, address(new AfterSwap()));
+        mc.use("BeforeAddLiquidity", BeforeAddLiquidity.beforeAddLiquidity.selector, address(new BeforeAddLiquidity()));
+        mc.use("BeforeRemoveLiquidity", BeforeRemoveLiquidity.beforeRemoveLiquidity.selector, address(new BeforeRemoveLiquidity()));
+        mc.use("BeforeSwap", BeforeSwap.beforeSwap.selector, address(new BeforeSwap()));
+        Reader reader = new Reader();
+        mc.use("beforeSwapCount", Reader.beforeSwapCount.selector, address(reader));
+        mc.use("afterSwapCount", Reader.afterSwapCount.selector, address(reader));
+        mc.use("beforeAddLiquidityCount", Reader.beforeAddLiquidityCount.selector, address(reader));
+        mc.use("beforeRemoveLiquidityCount", Reader.beforeRemoveLiquidityCount.selector, address(reader));
+        mc.useFacade(address(new CounterFacade()));
+        address dictionaryAddress = mc.deployDictionary().addr;
+        
+        // hook contracts must have specific flags encoded in the address
+        uint160 permissions = uint160(
+            Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG
+                | Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG
+        );
+
+        bytes memory initializeArgs = abi.encodeWithSelector(Base.validateHookAddress.selector);
+        
+        // Mine a salt that will produce a hook address with the correct permissions
+        (address hookAddress, bytes32 salt) =
+            HookMiner.find(address(this), permissions, type(UCSProxy).creationCode, abi.encode(dictionaryAddress, initializeArgs));
+
+        // ----------------------------- //
+        // Deploy the hook using CREATE2 //
+        // ----------------------------- //
+        UCSProxy counter = new UCSProxy{salt: salt}(dictionaryAddress, initializeArgs);
+        require(address(counter) == hookAddress, "CounterScript: hook address mismatch");
+        return address(counter);
+    }
+    receive() external payable override(MCTest, Fixtures) {}
 }
